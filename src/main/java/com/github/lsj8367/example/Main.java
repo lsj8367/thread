701d46d061000ed19a699a7bbcfaea843a3b9e6b
lsj8367 <lsj8367@naver.com>
2022-05-24 00:22:50 +0900

    :sparkles: Initial Commit

---
+++ src/main/java/com/github/lsj8367/example/Main.java
@@ -0,0 +1,20 @@
+package com.github.lsj8367.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Thread thread = new NewThread();
+
+        thread.start();
+    }
+
+    private static class NewThread extends Thread {
+
+        @Override
+        public void run() {
+            System.out.println("Hello from" + this.getName());
+        }
+
+    }
+
+}
